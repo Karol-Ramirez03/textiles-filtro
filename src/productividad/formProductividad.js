1bd2e429430d148854ca9694d361df35e5bf1c65
Karol-Ramirez03 <karol03112006@gmail.com>
2024-05-31 11:12:23 -0500

    productividad

--- src/productividad/formProductividad.js
+++ src/productividad/formProductividad.js
@@ -21,20 +21,15 @@ export class costosproductividad extends LitElement {
             <div class="card">
               <div class="card-body">
               <div class="card-body d-flex justify-content-between">
-                <h5 class="card-title">Registro de costos indirectos</h5>
-                <button type="button" class="btn btn-warning" id="addcosto" @click="${this.addCostField}">+</button>
+                <h5 class="card-title">Registro de productividad</h5>
               </div>
-                <form class="row g-3 divcostos" id="frmcosto">
+                <form class="row g-3 divcostos" id="frmproductividad">
                     <div class="col-md-8">
-                        <label for="productosterminados" class="form-label">porfavor ingresa los productos terminados</label>
-                        <input type="text" class="form-control" name="productosterminados" id="productosterminados">
-                    </div>
-                    <div class="col-md-2">
-                        <label for="valor" class="form-label">Valor del costo</label>
-                        <input type="number" class="form-control" name="valor" id="valor">
+                        <label for="productosterminados" class="form-label">porfavor ingresa los productos defectuosos</label>
+                        <input type="number" class="form-control" name="productosterminados" id="productosterminados">
                     </div>
                   <div class="col-12">
-                    <button type="submit" class="btn btn-primary" id="allcostos">Sign in</button>
+                    <button type="submit" class="btn btn-primary mandar" id="allcostos">Sign in</button>
                   </div>
                 </form>
               </div>
@@ -45,8 +40,17 @@ export class costosproductividad extends LitElement {
       </div>
     `;
   }
+  updated(){
+    const botondefect = this.shadowRoot.querySelector('.mandar')
+    botondefect.addEventListener('click', (e) => {
+        e.preventDefault()
+        const formulario = this.shadowRoot.querySelector('#frmproductividad')
+        const formdata = new  FormData(formulario)
+        const defectuosos = formdata.get('productosterminados') 
+        console.log(defectuosos)
+    })
 
-  
+  }
+
 }
 
-customElements.define('costos-productividad', costosproductividad);
